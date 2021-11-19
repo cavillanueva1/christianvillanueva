4d7c45b51a09413de1262b8e3e4cf8e9fc7a4fb1
cavillanueva1 <85634871+cavillanueva1@users.noreply.github.com>
2021-11-19 17:38:19 -0600

    onion 2

---
+++ blogdowncommands.R
@@ -0,0 +1,19 @@
+
+blogdown::new_site( theme = "victoriadrake/hugo-theme-sam")
+
+blogdown::hugo_version()
+
+blogdown::stop_server()
+
+blogdown::serve_site()
+
+blogdown::new_post(title = "I have absolutely no clue how to do this", ext = ".md")
+
+blogdown::new_post(title = "a novella", ext = ".md")
+
+blogdown::new_post(title = "wow look at all of my work wow im amazing", ext = ".md")
+
+blogdown::new_post(title = "click here", ext = ".md")
+
+blogdown::new_post(title = "this is a picture", ext = ".md")
+
---
+++ content/post/2021-11-19-to-the-onion-people/index.md
@@ -0,0 +1,11 @@
+---
+title: to the onion people
+author: R package build
+date: '2021-11-19'
+slug: []
+categories: []
+tags: []
+showDate: no
+---
+
+hello to all the onions out there sorry my website is work in progress i will eventually be doing blog posts and posting my thesis stuff up here but for the time being enjoy this glimpse into june 2021. anyway thanks for lookin hopefully im up to snuff
